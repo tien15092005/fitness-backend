5562f9bab3b7ac131d4076d259ae05906e12723a
Trần Quang Tiến <tranquangtien15092005@gmail.com>
2026-03-06 01:26:14 +0700

    Prepare for Vercel deploy

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l--r33h-&rhw8frmtt%sglny$tdg40=k3n^2-ejx-t1oq(+2@v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
 
 
 # Application definition
